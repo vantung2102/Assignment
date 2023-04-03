436762ca2d2c73a2430d3795015105ea52af19d3
Quoc Dat <1951120092@sv.ut.edu.vn>
2023-04-03 21:56:04 +0700

    push mobile code

--- Backend/config/initializers/cors.rb
+++ Backend/config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://zaneoffice.s3-website-ap-southeast-1.amazonaws.com'
+    origins 'localhost:3001'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: false
+      credentials: true
   end
 end
